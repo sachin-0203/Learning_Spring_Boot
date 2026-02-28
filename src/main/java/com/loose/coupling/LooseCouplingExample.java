e6f0e5f8a7803b70f81c5d18d813fb0d4effabd0
sachin <sachinprajapati0203@gmail.com>
2026-02-28 19:47:02 +0530

    Implementing IOC

--- src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -1,15 +1,24 @@
 package com.loose.coupling;
 
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class LooseCouplingExample {
 
     static void main() {
-        UserDataProvider userDataProvider = new UserDatabaseProvider();
-        UserManager userManagerWithDB = new UserManager(userDataProvider);
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationIOCLooseCouplingExample.xml");
+
+//        UserDataProvider userDataProvider = new UserDatabaseProvider();
+        UserManager userManagerWithDB = (UserManager) context.getBean("userManagerWithUserDatabaseProvider");
         System.out.println(userManagerWithDB.getUserInfo());
 
-        UserDataProvider userWebServiceProvider = new UserWebServiceProvider();
-        UserManager userManagerWithWS = new UserManager(userWebServiceProvider);
+//        UserDataProvider userWebServiceProvider = new UserWebServiceProvider();
+//        UserManager userManagerWithWS = new UserManager(userWebServiceProvider);
+//        System.out.println(userManagerWithWS.getUserInfo());
+
+        UserManager userManagerWithWS = (UserManager) context.getBean("userManagerWithUserWebService") ;
         System.out.println(userManagerWithWS.getUserInfo());
 
     }
